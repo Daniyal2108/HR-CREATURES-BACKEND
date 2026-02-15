3d862675ae402276940684880427a68c0e777323
daniyal-codeavenue <daniyal.khan@code-avenue.com>
2026-02-15 23:25:45 +0500

    syntax mistake

--- server.js
+++ server.js
@@ -33,14 +33,14 @@ mongoose
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
+  // server.close(() => {
+  //   process.exit(1);
+  // });
 });
 
 process.on('SIGTERM', () => {
   console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
-  server.close(() => {
-    console.log('💥 Process terminated!');
-  });
+  // server.close(() => {
+  //   console.log('💥 Process terminated!');
+  // });
 });
